8e05a2a62b4c1e293e2b2e52b65c2cc4355b5ed3
artem bystrov <ockidocki1@gmail.com>
2023-05-24 16:29:24 +0400

    testNG tests added

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,59 @@
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void amountPositive() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int expected = 100;
+        int actual = service.remain(amount);
+
+        org.testng.Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void amountNegative() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = -900;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void amountExactly() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void amountZero() {
+
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+}
+
+
